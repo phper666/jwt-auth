e2c41b77e9c94ceb51df994951c26798b868a200
Liyuzhao <YuzhaoLi@tmgworldwide.com>
2019-07-28 23:22:59 +0800

    init

---
+++ src/ConfigProvider.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://doc.hyperf.io
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf-cloud/hyperf/blob/master/LICENSE
+ */
+
+namespace Phper666\JwtAuth;
+
+class ConfigProvider
+{
+    public function __invoke(): array
+    {
+        return [
+            'dependencies' => [
+            ],
+            'commands' => [
+            ],
+            'scan' => [
+                'paths' => [
+                    __DIR__,
+                ],
+            ],
+            'publish' => [
+                [
+                    'id' => 'jwt',
+                    'description' => 'jwt-auth',
+                    'source' => __DIR__ . '/../publish/jwt.php',
+                    'destination' => BASE_PATH . '/config/autoload/jwt.php',
+                ],
+            ],
+        ];
+    }
+}
